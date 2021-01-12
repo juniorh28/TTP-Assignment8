46588660c457a8b2ff734a17266117d3098729b7
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-12 14:48:41 -0500

    add base code facilitate collaboration between members

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// components
+import { Table, TableCell, TableRow} from "./Components";
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div class="bar">
+            <ul>
+                <button class="addRowButton" onClick= {true}> Add Row </button>
+                <button class="addColButton" onClick= {true}> Add Column </button>
+                <button class="removeRowButton" onClick= {true}> Remove Row </button>
+                <button class="removeColButton" onClick= {true}> Remove Column </button>
+                <select onChange={true}>
+                    <option>Blue</option>
+                    <option>Red</option>
+                    <option>White</option>
+                    <option>Black</option>
+                    <option>Grey</option>
+                    <option>Yellow</option>
+                    <option>Red</option>
+                    <option>Blue</option>
+                    <option>Green</option>
+                    <option>Brown</option>
+                    <option>Pink</option>
+                    <option>Orange</option>
+                    <option>Purple</option>
+                </select>
+                <button onClick={true}> Clear </button>
+                <button class="fillAll" onclick= {true}> Fill All </button>
+                <button class="fillUncolored" onclick={true}> Fill Uncolored </button>
+            </ul>	
+            {/* <Table /> */}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
